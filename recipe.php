100526a753c15e9b77102f9174a054c7b5e66296
Jeff Thompson <mail@jeffreythompson.org>
2019-01-19 17:56:12 -0500

    Dynamic page title, pls

--- recipe.php
+++ recipe.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html profile="http://www.w3.org/2005/10/profile">
 <head>
+	<title>Recipe Book</title>
 
 	<!-- basics -->
 	<meta charset="UTF-8">
@@ -100,6 +101,11 @@
 						if (!foundTitle) {
 							id = 'title';
 							foundTitle = true;
+
+							// change page title too
+							var elems = $(section);
+							var pageTitle = elems[0].innerHTML + ' | Recipe Book';
+							$(document).prop('title', pageTitle);
 						}
 						// for all others, get id from regex match
 						else {
